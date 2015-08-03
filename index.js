1f8f3a78a00a236eae01357b871212b60a1a543f
Felix J. Acero <acerosalazar@gmail.com>
2015-08-03 15:56:12 +0200

    minor fix

--- index.js
+++ index.js
@@ -1,112 +1,33 @@
 
-var async        = require('async');
+
 var mongoose     = require('mongoose');
 var _            = require("underscore");
-var fs           = require("fs");
+var main          = requie("./main");
 
-var logger       = require("./logger.js");
-var crawler      = require("./crawler.js");
-var queryBuilder = require("./query-builder.js");
 
-var Tweet = require('./models/tweet.js');
-var Seed = require('./models/seed.js');
-var db = undefined;
+// Retrive environment variables
+var mongoProtocolHostAndPort  = process.env.MONGO_DB_PORT;
+var mongoDB                   = process.env.SKE_DATABASE_NAME;
+var crawlerStartDate          = process.env.SKE_CRAWLER_START_DATE;
 
-var connect = function(mongoHostAndPort, mongoDB) {
-
+// Check if environment variables have been set
+if(!_.isUndefined(mongoProtocolHostAndPort) && !_.isUndefined(mongoDB) && !_.isUndefined(crawlerStartDate)){
+  
+  // Slice out the Protocol prefix - i.e tcp://
+  var mongoHostAndPort = mongoProtocolHostAndPort.slice(_.lastIndexOf(mongoProtocolHostAndPort, "/") +1 );
+  
+  // Start database
   logger.debug('#index - Connecting to the database');
   mongoose.connect("mongodb://"+mongoHostAndPort+"/"+mongoDB);
   db = mongoose.connection; 
   
   db.once('open', function() {
-
-    start();
+    
+    main.start(db, crawlerStartDate);
   });
-};
-
-var start = function(){
-
-  async.waterfall([retrieveSeeds, prepareQueries, retrieveAndSaveTweets], function(err, result){
-    
-    if(err) {
-
-      logger.debug("#index " + errr);
-      process.exit();
-    } 
-   
-    
-    logger.debug("#index - Finished succesfully")
-    process.exit();
-  });
-};
-
-// 1. 
-var retrieveSeeds = function(callback){
-  
-  Seed.find(callback);
-};
-
-// 2.
-var prepareQueries = function(seeds, callback){
-
-  console.log("Seeds " + seeds);
-  console.log("Crawler Date" + crawlerStartDate);
-  console.log(queryBuilder);
-  var queries = queryBuilder.prepareQueriesForSeeds(seeds, crawlerStartDate);
-  callback(null, queries);
-};
-
-// 3. 
-var retrieveAndSaveTweets = function(queries, callback){
-  
-  async.each(queries, function(query, firstCallback){
-
-    async.each(query.twitterQueries, function(twitterQuery, secondCallback){
-        
-        var partialTwitterCrawler = _.partial(crawler.scrapeTweetsFromSearchResult, twitterQuery);
-        var partialTwitterSaver   = _.partial(saveTweets, _, query.seedId );
-
-        async.waterfall([partialTwitterCrawler, partialTwitterSaver], secondCallback);
-    }, firstCallback);
-
-  }, callback);
-};
-
-// 4. 
-var saveTweets = function(tweets, seedId, callback){
-
-  if(tweets && tweets.length > 0){
-
-    var Tweet = db.model('tweet');
-
-    _.each(tweets, function(tweet){
-      
-      tweet.seed = seedId;
-    });
-
-    logger.debug('#index - Saving ' + tweets.length + ' tweets for seed: ' + seedId);
-
-    return Tweet.create(tweets, callback);
-  } 
-  else {
-
-    logger.debug('#index - Could not find any tweets related with seed: ' + seedId);
-    callback();
-  }
-};
-
-
-// Retrive environment variables
-var mongoProtocolHostAndPort = process.env.MONGO_DB_PORT;
-var mongoDB = process.env.SKE_DATABASE_NAME;
-var crawlerStartDate  = process.env.SKE_CRAWLER_START_DATE;
-
-if(!_.isUndefined(mongoProtocolHostAndPort) && !_.isUndefined(mongoDB) && !_.isUndefined(crawlerStartDate)){
-    
-  var mongoHostAndPort = mongoProtocolHostAndPort.slice(_.lastIndexOf(mongoProtocolHostAndPort, "/") +1 );
-  connect(mongoHostAndPort, mongoDB);
 }
 else {
-  logger.debug("#index - environment Variables not set")
+
+  logger.debug("#index - environment variables not set")
   process.exit();
 }
---
+++ main.js
@@ -0,0 +1,80 @@
+
+var async        = require('async');
+var logger       = require("./logger");
+var crawler      = require("./crawler");
+var queryBuilder = require("./query-builder");
+
+var Tweet = require('./models/tweet.js');
+var Seed = require('./models/seed.js');
+
+// 1. 
+var retrieveSeeds = function(callback){
+  
+  Seed.find(callback);
+};
+
+// 2.
+var prepareQueries = function(seeds, crawlerStartDate, callback){
+
+  var queries = queryBuilder.prepareQueriesForSeeds(seeds, crawlerStartDate);
+  callback(null, queries);
+};
+
+// 3. 
+var retrieveAndSaveTweets = function(queries, db, callback){
+  
+  async.each(queries, function(query, firstCallback){
+
+    async.each(query.twitterQueries, function(twitterQuery, secondCallback){
+        
+        var partialTwitterCrawler = _.partial(crawler.scrapeTweetsFromSearchResult, twitterQuery);
+        var partialTwitterSaver   = _.partial(saveTweets, _, query.seedId, db);
+
+        async.waterfall([partialTwitterCrawler, partialTwitterSaver], secondCallback);
+    }, firstCallback);
+
+  }, callback);
+};
+
+// 4. 
+var saveTweets = function(tweets, seedId, db, callback){
+
+  if(tweets && tweets.length > 0){
+
+    var Tweet = db.model('tweet');
+
+    _.each(tweets, function(tweet){
+      
+      tweet.seed = seedId;
+    });
+
+    logger.debug('#index - Saving ' + tweets.length + ' tweets for seed: ' + seedId);
+
+    return Tweet.create(tweets, callback);
+  } 
+  else {
+
+    logger.debug('#index - Could not find any tweets related with seed: ' + seedId);
+    callback();
+  }
+};
+
+exports = module.exports = {
+  
+  start:function(db, crawlerStartDate){
+
+    var pRetrieveAndSaveTweets = _.partial(retrieveAndSaveTweets, _, db);
+    var pPrepareQueries = _.partial(prepareQueries, _, crawlerStartDate);
+    async.waterfall([retrieveSeeds, pPrepareQueries, pRetrieveAndSaveTweets], function(err, result){
+      
+      if(err) {
+
+        logger.debug("#index " + errr);
+        process.exit();
+      } 
+     
+      logger.debug("#index - Finished succesfully")
+      process.exit();
+    });
+  }
+};
