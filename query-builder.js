381a6874bb8bcde22d8f18d753a0c737e8eb86b0
Felix J. Acero <acerosalazar@gmail.com>
2015-08-03 15:09:03 +0200

    minor fix

--- query-builder.js
+++ query-builder.js
@@ -1,38 +1,5 @@
 var _ = require("underscore");
 
-
-// Prepares the query strings needed to obtain social media content
-// from a set of social networks ( currenty only Twitter ).
-// @socialSeeds - An array with <Seed> objects
-// @sinceDate - The earliest day from which the social media content will be gathered
-// @return - An array of <Query> objtects
-var prepareQueriesForSeeds = function(seeds, sinceDate){
-
-	var queries = [];
-
-	_.each(seeds, function(seed){
-		
-		var seedQueries = [];
-
-		_.each(seed.twitter, function(twitterTerm) {
-
-			if(twitterTerm[0] === "@"){
-
-				seedQueries.push(buildTwitterQueryWithAuthor(twitterTerm, sinceDate));
-			}
-
-			seedQueries.push(buildTwitterQueryWithTerm(twitterTerm, sinceDate));
-		});
-
-		queries.push({
-			'seedId':seed._id,
-			'twitterQueries':seedQueries
-		});
-	});
-
-	return queries;
-};
-
 var buildTwitterQueryWithTerm = function(searchTerm, sinceDate){
 	
 	return {
@@ -51,4 +18,37 @@ var buildTwitterQueryWithAuthor = function(author, sinceDate){
   	};
 };
 
-exports = module.exports = {'prepareQueriesForSeeds':prepareQueriesForSeeds}
+exports = module.exports = {
+
+	// Prepares the query strings needed to obtain social media content
+	// from a set of social networks ( currenty only Twitter ).
+	// @socialSeeds - An array with <Seed> objects
+	// @sinceDate - The earliest day from which the social media content will be gathered
+	// @return - An array of <Query> objtects
+	prepareQueriesForSeeds:function(seeds, sinceDate){
+
+		var queries = [];
+
+		_.each(seeds, function(seed){
+			
+			var seedQueries = [];
+
+			_.each(seed.twitter, function(twitterTerm) {
+
+				if(twitterTerm[0] === "@"){
+
+					seedQueries.push(buildTwitterQueryWithAuthor(twitterTerm, sinceDate));
+				}
+
+				seedQueries.push(buildTwitterQueryWithTerm(twitterTerm, sinceDate));
+			});
+
+			queries.push({
+				'seedId':seed._id,
+				'twitterQueries':seedQueries
+			});
+		});
+
+		return queries;
+	}
+};
